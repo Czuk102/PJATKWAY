cfc7134525d0107a8a21d8418ab5e0e735d779ba
Czuk102 <czuk102@gmail.com>
2021-11-07 20:45:12 +0100

    Service działa, requesty też, podłączona baza H2

--- src/main/java/com/example/PJATKWAY/Ingredient.java
+++
@@ -1,7 +0,0 @@
-package com.example.PJATKWAY;
-
-public class Ingredient {
-    private String name;
-    private int calories;
-
-}
--- src/main/java/com/example/PJATKWAY/Size.java
+++
@@ -1,3 +0,0 @@
-package com.example.PJATKWAY;
-
-enum Size {SMALL, MEDIUM, LARGE, }
